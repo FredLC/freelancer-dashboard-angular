6e15e7ed727782eaa2df721d642b89b9d23a5dcf
fredlc <fredlefevre@outlook.com>
2020-06-06 18:02:13 -0400

    Built document interface and generated mock data

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   selector: 'documents',
   templateUrl: './documents.component.html'
 })
-export class DocumentsComponent { }
+export class DocumentsComponent { 
+	documents: Document[] = [
+		{
+			title: 'My Great Doc 1',
+			description: 'asdf',
+			file_url: 'http://google.com',
+			image_url: 'http://google.com',
+			updated_at: '6/6/20'
+		},
+		{
+			title: 'My Great Doc 2',
+			description: 'asdf',
+			file_url: 'http://google.com',
+			image_url: 'http://google.com',
+			updated_at: '6/6/20'
+		},
+		{
+			title: 'My Great Doc 3',
+			description: 'asdf',
+			file_url: 'http://google.com',
+			image_url: 'http://google.com',
+			updated_at: '6/6/20'
+		}
+	]
+}

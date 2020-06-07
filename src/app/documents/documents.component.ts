90f97d24df682969cdf7bfc40ba580c98245bf3d
fredlc <fredlefevre@outlook.com>
2020-06-07 18:40:09 -0400

    Card layout for documents component

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -11,21 +11,21 @@ export class DocumentsComponent {
 			title: 'My Great Doc 1',
 			description: 'asdf',
 			file_url: 'http://google.com',
-			image_url: 'http://google.com',
+			image_url: 'https://images.unsplash.com/photo-1517028267-bcc3eba7474c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80',
 			updated_at: '6/6/20'
 		},
 		{
 			title: 'My Great Doc 2',
 			description: 'asdf',
 			file_url: 'http://google.com',
-			image_url: 'http://google.com',
+			image_url: 'https://images.unsplash.com/photo-1517028267-bcc3eba7474c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80',
 			updated_at: '6/6/20'
 		},
 		{
 			title: 'My Great Doc 3',
 			description: 'asdf',
 			file_url: 'http://google.com',
-			image_url: 'http://google.com',
+			image_url: 'https://images.unsplash.com/photo-1517028267-bcc3eba7474c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80',
 			updated_at: '6/6/20'
 		}
 	]

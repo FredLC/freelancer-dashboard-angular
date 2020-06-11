03836bb10978588332d1f423b53cb1407f76188f
fredlc <fredlefevre@outlook.com>
2020-06-11 14:24:17 -0400

    Implemented call to retrieve singular proposal and populate show page

--- src/app/proposal/proposal-show.component.ts
+++ src/app/proposal/proposal-show.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
 	selector: 'proposal-show',
-	templateUrl: './proposal-show.component.html'
+	templateUrl: './proposal-show.component.html',
+	providers: [ProposalService]
 })
 
 export class ProposalShowComponent implements OnInit {
-	id: number;
-	routeId: any;
 
 	constructor(
-		private route: ActivatedRoute
+		private http: HttpClient,
+		private route: ActivatedRoute,
+		private proposalService: ProposalService
 	) {}
 
+	@Input()
+	proposal: Proposal;
+
+
 	ngOnInit(): void {
-		this.routeId = this.route.params.subscribe(
-			params => {
-				this.id = +params['id'];
-			}
-		)
+		let proposalRequest = this.route.params.pipe(
+		flatMap((params: Params) => this.proposalService.getProposal(+params['id'])));
+		proposalRequest.subscribe(response => this.proposal = response);
 	}
 }
--- src/app/proposal/proposal.service.ts
+++ src/app/proposal/proposal.service.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';	
 import { Proposal } from './proposal';
 
 @Injectable()
 export class ProposalService {
-	private proposalsUrl = 'http://localhost:3001/proposals.json';
+	private proposalsUrl = 'http://localhost:3001/proposals';
 
 	constructor (
 		private http: HttpClient
 	) {}
 
 	getProposals(): Observable<Proposal[]> {
-		return this.http.get<Proposal[]>(this.proposalsUrl)
+		return this.http.get<Proposal[]>(this.proposalsUrl + '.json')
       .pipe(
         catchError(this.handleError<Proposal[]>('proposals', []))
       )
+	}
+
+	getProposal(id: number): Observable<Proposal> {
+		return this.http.get<Proposal>(this.proposalsUrl + '/' + id + '.json')
 	}
 
 	private handleError<T>(operation = 'operation', result?: T) {
